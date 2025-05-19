b606cdb726555f1731e12e56ef1897454a0a5f9c
--replace <your-email@example.com>
2025-05-19 22:23:32 +0200

    Background color van form1 veranderd

--- EmberPlusWinForms/Form1.Designer.cs
+++ EmberPlusWinForms/Form1.Designer.cs
@@ -685,6 +685,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
             ClientSize = new Size(1248, 745);
             Controls.Add(button21);
             Controls.Add(button22);
