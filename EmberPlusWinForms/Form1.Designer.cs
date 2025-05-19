019affc5769e02edb95ff6db2bf20b7e4dad87d0
--replace <your-email@example.com>
2025-05-19 22:33:07 +0200

    Backcolor form1 lichtgeel gemaakt

--- EmberPlusWinForms/Form1.Designer.cs
+++ EmberPlusWinForms/Form1.Designer.cs
@@ -685,7 +685,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.LightBlue;
+            BackColor = Color.LemonChiffon;
             ClientSize = new Size(1248, 745);
             Controls.Add(button21);
             Controls.Add(button22);
